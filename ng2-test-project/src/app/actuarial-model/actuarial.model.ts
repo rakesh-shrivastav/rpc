963910c50e4b2631a9e042acc6ffa840aa4ec5c3
rakesh-shrivastav <rakesh.shrivastav@gmail.com>
2019-09-13 09:13:24 +0100

    Update actuarial.model.ts

--- ng2-test-project/src/app/actuarial-model/actuarial.model.ts
+++ ng2-test-project/src/app/actuarial-model/actuarial.model.ts
@@ -1,16 +1,1 @@
-import { TypeOfExecution } from './TypeOfExecution.enum';
-import { Injectable } from '@angular/core';
 
-@Injectable()
-export class ActuarialModel {
-	MainLimit: number;
-	MainRetention: number;
-	ExecutionType: TypeOfExecution;
-	FinalValue: number;
-	ExecutionDate: Date
-}
-
-@Injectable()
-export class ActuarialResult {
-	Result: ActuarialModel[] = [];
-}
